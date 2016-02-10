454a2f2f5a3cae549118887820d2297a184b3261
djres88 <davidj.resnick@gmail.com>
2016-02-10 12:33:28 -0800

    Moving day 2 items to day 2

---
+++ day_2/day_2.rb
@@ -0,0 +1,17 @@
+#https://github.com/Haseeb-Qureshi/jump-start/blob/master/d2.md
+#CODE STYLE EXERCISES:
+
+# The below is valid and correct Ruby. However, the style sucks. See if you can fix the style problems and variable naming.
+
+# def  hello_world
+# puts "Hello, world!" end
+# def is_prime num
+# n = 2
+# while n < num
+#     if (num%n==0)
+#       return (false)
+#     end
+#     n = n + 1
+#   end
+#   return true
+# end
